a25fb62bb854c91e91b42770f1b6ea210be50a82
febritecno <febritecno0@yahoo.com>
2018-05-27 21:43:47 +0700

    memulai

---
+++ application/views/includes/footer.php
@@ -0,0 +1,29 @@
+
+
+    <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Garuda Informatics</b>
+        </div>
+        <strong>Copyright &copy; 2018 <a href="<?php echo base_url(); ?>">UNWAHA | Garuda Informatics</a>.</strong> All rights reserved.
+    </footer>
+    
+    <!-- jQuery UI 1.11.2 -->
+    <!-- <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.min.js" type="text/javascript"></script> -->
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <!-- Bootstrap 3.3.2 JS -->
+    <script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>assets/dist/js/app.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery.validate.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>assets/js/validation.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        var windowURL = window.location.href;
+        pageURL = windowURL.substring(0, windowURL.lastIndexOf('/'));
+        var x= $('a[href="'+pageURL+'"]');
+            x.addClass('active');
+            x.parent().addClass('active');
+        var y= $('a[href="'+windowURL+'"]');
+            y.addClass('active');
+            y.parent().addClass('active');
+    </script>
+  </body>
+</html>
